36fe2ceb211a6de42f0e1abcd8c5becc3ef9e936
CarlsonC09 <carlsonchuang@gmail.com>
2025-05-28 17:58:13 +0800

    man

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,25 @@ module.exports = defineConfig({
 
   // Configure the build output
   chainWebpack: config => {
+    // Handle static assets
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => {
+        options = options || {}
+        options.limit = 4096
+        options.fallback = {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[hash:8].[ext]',
+            publicPath: process.env.NODE_ENV === 'production' ? '/maimatch_web2/' : '/'
+          }
+        }
+        return options
+      })
+
+    // Configure HTML plugin
     config
       .plugin('html')
       .tap(args => {
@@ -24,6 +43,8 @@ module.exports = defineConfig({
             content: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
           }
         }
+        // Configure base URL for assets
+        args[0].base = process.env.NODE_ENV === 'production' ? '/maimatch_web2/' : '/'
         return args
       })
   }
